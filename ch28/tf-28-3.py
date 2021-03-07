070e2d454ddc7f55fbbbcab74b14fd52d1391254
James Yoo <jyoo980@gmail.com>
2021-03-07 11:58:17 -0800

    adding implementation for iterator-based exercise (28.3)

--- ch28/tf-28-3.py
+++ ch28/tf-28-3.py
@@ -1,47 +1,56 @@
 #!/usr/bin/env python
 import sys, operator, string
 
-def characters(filename):
-    for line in open(filename):
-        for c in line:
-            yield c
+def line_iterator(filename):
+    lines = [line for line in open(filename)]
+    return iter(lines)
 
-def all_words(filename):
-    start_char = True
-    for c in characters(filename):
-        if start_char == True:
-            word = ""
-            if c.isalnum():
-                # We found the start of a word
-                word = c.lower()
-                start_char = False
-            else: pass
+def all_words_iterator(filename):
+    line_iter = line_iterator(filename)
+    words = []
+    at_end = False
+    while not at_end:
+        line = next(line_iter, None)
+        if line is not None:
+            words.extend(line.split(" "))
         else:
-            if c.isalnum():
-                word += c.lower()
-            else:
-                # We found end of word, emit it
-                start_char = True
-                yield word
+            at_end = True
+    return iter([w for w in words if w.isalnum()])
 
-def non_stop_words(filename):
+def non_stop_words_iter(filename):
     stopwords = set(open('../stop_words.txt').read().split(',')  + list(string.ascii_lowercase))
-    for w in all_words(filename):
-        if not w in stopwords:
-            yield w
+    word_iter = all_words_iterator(filename)
+    valid_words = []
+    at_end = False
+    while not at_end:
+        word = next(word_iter, None)
+        if word is not None and word not in stopwords:
+            valid_words.append(word)
+        elif word is None:
+            at_end = True
+    return iter(valid_words)
 
-def count_and_sort(filename):
+def count_and_sort_iter(filename):
     freqs, i = {}, 1
-    for w in non_stop_words(filename):
-        freqs[w] = 1 if w not in freqs else freqs[w]+1
-        if i % 5000 == 0:
-            yield sorted(freqs.items(), key=operator.itemgetter(1), reverse=True)
-        i = i+1
-    yield sorted(freqs.items(), key=operator.itemgetter(1), reverse=True)
+    words_iter = non_stop_words_iter(filename)
+    at_end = False
+    while not at_end:
+        word = next(words_iter, None)
+        if word is not None:
+            freqs[word] = freqs.get(word, 0) + 1
+            if i % 5000 == 0:
+                sorted_entries = sorted(freqs.items(), key=operator.itemgetter(1), reverse=True)
+                return iter(sorted_entries)
+            i = i + 1
+        else:
+            at_end = True
+    sorted_entries = sorted(freqs.items(), key=operator.itemgetter(1), reverse=True)
+    return iter(sorted_entries)
+
 #
 # The main function
 #
-for word_freqs in count_and_sort(sys.argv[1]):
-    print("-----------------------------")
-    for (w, c) in word_freqs[0:25]:
-        print(w, '-', c)
+word_freq_iter = count_and_sort_iter(sys.argv[1])
+for _ in range(0, 26):
+    (w, c) = next(word_freq_iter)
+    print(f"{w}-{c}")
