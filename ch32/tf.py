a2f015fe6d395a3c2a8e698b37282f0f0a4c53c7
James Yoo <jyoo980@gmail.com>
2021-03-31 00:16:25 -0700

    adding initial conversion to larger buckets

--- ch32/tf.py
+++ ch32/tf.py
@@ -36,6 +36,25 @@ def split_words(data_str):
         result.append((w, 1))
     return result
 
+# At worst, this is linear time. I could make this O(1)
+# by hashing each letter to its range, but I'm not really
+# down to make a hashmap with 26 key-value pairs.
+def find_key_of(word):
+    letters = list(string.ascii_lowercase)
+    first_char = word[0]
+    key = ""
+    if first_char in letters[0:5]:
+        key = "a-e"
+    elif first_char in letters[5:10]:
+        key = "f-j"
+    elif first_char in letters[10:15]:
+        key = "k-o"
+    elif first_char in letters[15:20]:
+        key = "p-t"
+    else:
+        key = "u-z"
+    return key
+
 def regroup(pairs_list):
     """
     Takes a list of lists of pairs of the form
@@ -44,29 +63,29 @@ def regroup(pairs_list):
      ...]
     and returns a dictionary mapping each unique word to the
     corresponding list of pairs, so
-    { w1 : [(w1, 1), (w1, 1)...],
-      w2 : [(w2, 1), (w2, 1)...],
+    { a-e: [(w1, 1), (w1, 1)...],
+      f-j: [(w2, 1), (w2, 1)...],
       ...}
     """
     mapping = {}
-    for pairs in pairs_list:
-        for p in pairs:
-            if p[0] in mapping:
-                mapping[p[0]].append(p)
+    for pair in pairs_list:
+        for p in pair:
+            word = p[0]
+            key = find_key_of(word)
+            if key in mapping:
+                mapping[key].append(p)
             else:
-                mapping[p[0]] = [p]
+                mapping[key] = [p]
     return mapping
 
-def count_words(mapping):
-    """
-    Takes a mapping of the form (word, [(word, 1), (word, 1)...)])
-    and returns a pair (word, frequency), where frequency is the
-    sum of all the reported occurrences
-    """
-    def add(x, y):
-        return x+y
+def count_words(target, mappings):
+    counts = sum([freq for word, freq in mappings if word == target])
+    return (target, counts)
 
-    return (mapping[0], reduce(add, (pair[1] for pair in mapping[1])))
+def map_over_bucket(mapping):
+    # given the second part of "a-e": [ (w1, 1), (w2, 2) ... ]
+    unique_words = { w for w, _ in mapping }
+    return [count_words(word, mapping) for word in unique_words]
 
 #
 # Auxiliary functions
@@ -84,7 +103,13 @@ def sort(word_freq):
 #
 splits = map(split_words, partition(read_file(sys.argv[1]), 200))
 splits_per_word = regroup(splits)
-word_freqs = sort(map(count_words, splits_per_word.items()))
+
+buckets = splits_per_word.items()
+counted_buckets = []
+for _, words in buckets:
+    counted_buckets.extend(map_over_bucket(words))
+
+word_freqs = sort(counted_buckets)
 
 for (w, c) in word_freqs[0:25]:
     print(w, '-', c)
